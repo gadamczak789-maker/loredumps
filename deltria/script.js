d7121b4e1a5cef6c8c481071171db1d31c622edf
gadamczak789-maker <gadamczak789@gmail.com>
2026-05-13 15:09:33 -0500

    Update script.js

--- deltria/script.js
+++ deltria/script.js
@@ -1,23 +1,115 @@
-const message = document.getElementById("message");
+const hexContainer = document.getElementById("hexContainer");
 
-function showMessage() {
+/* Full hexadecimal sequence */
 
-    // Show after 303 seconds
-    setTimeout(() => {
+const hexSequence = [
+    "41",
+    "6E",
+    "74",
+    "65",
+    "72",
+    "6F",
+    "20",
+    "69",
+    "73",
+    "20",
+    "61",
+    "6C",
+    "69",
+    "76",
+    "65",
+    "0A"
+];
 
-        message.style.opacity = 1;
+/* Random position generator */
 
-        // Hide after 27 seconds
-        setTimeout(() => {
+function randomPosition(element) {
 
-            message.style.opacity = 0;
+    const maxX = window.innerWidth - 200;
+    const maxY = window.innerHeight - 100;
 
-            // Restart loop
-            showMessage();
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
 
-        }, 27000);
-
-    }, 303000);
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px`;
 }
 
-showMessage();
+/* Create flashing text */
+
+function createHex(code) {
+
+    const div = document.createElement("div");
+
+    div.classList.add("hexText");
+
+    div.textContent = code;
+
+    randomPosition(div);
+
+    hexContainer.appendChild(div);
+
+    /* Fade in */
+
+    setTimeout(() => {
+        div.style.opacity = 1;
+    }, 100);
+
+    /* Fade out after 5 seconds */
+
+    setTimeout(() => {
+
+        div.style.opacity = 0;
+
+        setTimeout(() => {
+            div.remove();
+        }, 1500);
+
+    }, 5000);
+}
+
+/* Start sequence loop */
+
+function startSequenceLoop() {
+
+    let index = 0;
+
+    function nextSequence() {
+
+        createHex(hexSequence[index]);
+
+        index++;
+
+        if (index >= hexSequence.length) {
+            index = 0;
+        }
+
+        /* Wait 10 seconds total:
+           5 visible
+           5 hidden */
+
+        setTimeout(nextSequence, 10000);
+    }
+
+    nextSequence();
+}
+
+/* Initial delay */
+
+setTimeout(() => {
+
+    startSequenceLoop();
+
+}, 15000);
+
+/* Random subtle page glitches */
+
+setInterval(() => {
+
+    document.body.classList.add("glitch");
+
+    setTimeout(() => {
+        document.body.classList.remove("glitch");
+    }, 120);
+
+}, 4000 + Math.random() * 5000);
